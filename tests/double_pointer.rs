ec828fce428f6b54b3431f7f5255d3cfb6a22a68
haitmt <taminhtienhai@gmail.com>
2024-03-31 15:24:05 +0700

    test: try #[reflection] macro attribute

--- tests/double_pointer.rs
+++ tests/double_pointer.rs
@@ -1,7 +1,10 @@
+use json_parser_macro::reflection;
+
 
 
 #[test]
 fn two_pointer() {
+    #[allow(dead_code)]
     struct Ptr(u8);
     let ptr = Ptr(10);
 
@@ -11,4 +14,17 @@ fn two_pointer() {
 
     println!("{:?}", ptr_01);
     println!("{:?}", ptr_02);
+}
+
+#[test]
+fn reflection_macro() {
+    #[reflection]
+    fn sum(a: usize, b: usize) -> usize {
+        println!("{}", __fn_ident);
+        println!("{}", __arg_0);
+        println!("{}", __arg_1);
+        a + b
+    }
+
+    let _ = sum(1,2);
 }
